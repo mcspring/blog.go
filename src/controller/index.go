d11010753fc47c0a927686f8c6ec773156f65f6d
Mc.Spring <Heresy.Mc@gmail.com>
2011-01-11 14:26:18 -0500

    setup mustache.go template support

--- src/controller/index.go
+++ src/controller/index.go
@@ -1,5 +1,20 @@
 package controller
 
+import (
+	"os"
+	"path"
+	library "../library/_obj/library"
+)
+
 func IndexHandler() string {
-    return "Hello, Blog.go!"
+	root := os.Getenv("cd ..; pwd")
+	view := path.Join(root, "view")
+	file := path.Join(path.Join(view, "index"), "index.mustache")
+
+	data := map[string] string {
+		"title": "Hello, blog.go",
+		"name": "Welcome to blog.go!",
+	}
+
+    return library.RenderFile(file, data)
 }
